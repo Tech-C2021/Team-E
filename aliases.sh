d06f043c489f542aab32950919704ac1a8316097
sabir <sabir_barahi4@hotmail.com>
2022-01-27 23:47:32 +0900

    updated aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,10 @@
-alias s-build="docker-compose build"
-alias scraper="docker-compose up"
+alias s-build="docker-compose build server"
+alias s-scraper="docker-compose up server"
 alias s-bash="docker exec -it scraping /bin/bash"
 alias scrape="docker exec -it scraping ruby main.rb"
 alias sp="docker-compose down && docker-compose build && docker-compose up"
+alias c-build="docker-compose build client"
+alias c-start="docker-compose up client"
+alias c-bash="docker exec -it client /bin/bash"
+
 
