a2bdd6541771d121eb2d5b501d6762aaad12ce7d
sabir <sabir_barahi4@hotmail.com>
2022-01-28 00:22:41 +0900

    initial client environment

--- aliases.sh
+++ aliases.sh
@@ -4,7 +4,7 @@ alias s-bash="docker exec -it scraping /bin/bash"
 alias scrape="docker exec -it scraping ruby main.rb"
 alias sp="docker-compose down && docker-compose build && docker-compose up"
 alias c-build="docker-compose build client"
-alias c-start="docker-compose up client"
-alias c-bash="docker exec -it client /bin/bash"
+alias c-start="docker-compose up scrapingClient"
+alias c-bash="docker exec -it scrapingClient /bin/bash"
 
 
