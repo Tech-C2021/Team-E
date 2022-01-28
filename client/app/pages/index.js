b4474bc3146960ab8e9df7cc756791e372baef5f
sabir <sabir_barahi4@hotmail.com>
2022-01-29 06:05:19 +0900

    added styles for main page

--- client/app/pages/index.js
+++ client/app/pages/index.js
@@ -15,12 +15,24 @@ return (
         <input placeholder="Enter the hero you want to track" className="trackerInput"></input>
         <button className="trackerButton">Track</button>
       </form>
+      <div className="grid">      { heroes.map(hero => (
+        <div key = { hero.id } className="trackedHeroes">
+          <img src={ hero.icon } className="icon"></img>
+          <p className="name">{ hero.name }</p>
+          <p>Tier 1 Pick Rate: { hero.lowTierPickRate }%</p>
+          <p>Tier 1 Win Rate: { hero.lowTierWinRate }%</p>
+          <p>Tier 2 Pick Rate: { hero.beginnerTierPickRate }%</p>
+          <p>Tier 2 Win Rate: { hero.beginnerTierPickRate }%</p>
+          <p>Tier 3 Pick Rate: { hero.intermediateTierPickRate }%</p>
+          <p>Tier 3 Win Rate: { hero.intermediateTierPickRate }%</p>
+          <p>Tier 4 Pick Rate: { hero.topTierPickRate }%</p>
+          <p>Tier 4 Win Rate: { hero.topTierPickRate }%</p>
 
-      { heroes.map(hero => (
-        <div key = { hero.id }>
-          <p>{ hero.name }</p>
+
         </div> 
       )) }
+    </div>
+
     </div>
       )
 }
